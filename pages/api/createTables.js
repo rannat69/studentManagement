48d9f40f5c966f88e019ac781a650f195f0fcf4b
Remi ANNAT <remi_annat@hotmail.com>
2026-01-02 11:22:36 +0800

    Change password

--- pages/api/createTables.js
+++ pages/api/createTables.js
@@ -1,22 +1,20 @@
 export function createTables(db, bcrypt) {
+  console.log("create tables ");
 
-    console.log("create tables ");
+  db.serialize(() => {
+    //db.run(`DROP TABLE IF EXISTS student`);
+    //db.run(`DROP TABLE IF EXISTS request`);
+    //db.run(`DROP TABLE IF EXISTS student_course`);
+    //	db.run(`DROP TABLE IF EXISTS qualification`);
+    //db.run(`DROP TABLE IF EXISTS course`);
+    //db.run(`DELETE FROM student_course`);
+    //db.run(`DELETE FROM request`);
+    //db.run(`DROP TABLE IF EXISTS user`);
+  });
 
-    db.serialize(() => {
-        //db.run(`DROP TABLE IF EXISTS student`);
-        //db.run(`DROP TABLE IF EXISTS request`);
-        //db.run(`DROP TABLE IF EXISTS student_course`);
-        //	db.run(`DROP TABLE IF EXISTS qualification`);
-
-        //db.run(`DROP TABLE IF EXISTS course`);
-        //db.run(`DELETE FROM student_course`);
-        //db.run(`DELETE FROM request`);
-        //db.run(`DROP TABLE IF EXISTS user`);
-    });
-
-    // Create the student table
-    db.serialize(() => {
-        db.run(`CREATE TABLE IF NOT EXISTS student (
+  // Create the student table
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS student (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         student_number INTEGER,
             l_name TEXT,
@@ -32,8 +30,8 @@ export function createTables(db, bcrypt) {
             
     )`);
 
-        // course
-        db.run(`CREATE TABLE IF NOT EXISTS course (
+    // course
+    db.run(`CREATE TABLE IF NOT EXISTS course (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             hkust_identifier TEXT,
             name TEXT,
@@ -47,7 +45,7 @@ export function createTables(db, bcrypt) {
             keywords TEXT
                     )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS student_course (
+    db.run(`CREATE TABLE IF NOT EXISTS student_course (
             student_id INTEGER,
             course_id INTEGER,
             year INTEGER,
@@ -56,7 +54,7 @@ export function createTables(db, bcrypt) {
             FOREIGN KEY (course_id) REFERENCES course(id)
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS teacher (
+    db.run(`CREATE TABLE IF NOT EXISTS teacher (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                                l_name TEXT,
             f_names TEXT,
@@ -65,7 +63,7 @@ export function createTables(db, bcrypt) {
    
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS request (
+    db.run(`CREATE TABLE IF NOT EXISTS request (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
                student_id INTEGER,
                         teacher_id INTEGER,
@@ -80,90 +78,88 @@ FOREIGN KEY (course_id) REFERENCES course(id)
 
  )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS course_area (
+    db.run(`CREATE TABLE IF NOT EXISTS course_area (
             area TEXT,
             course_id INTEGER,           
             FOREIGN KEY (course_id) REFERENCES course(id)
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS course_qualification (
+    db.run(`CREATE TABLE IF NOT EXISTS course_qualification (
             qualification TEXT,
             course_id INTEGER,           
             FOREIGN KEY (course_id) REFERENCES course(id)
         )`);
 
-
-
-        db.run(`CREATE TABLE IF NOT EXISTS student_area (
+    db.run(`CREATE TABLE IF NOT EXISTS student_area (
             student_id INTEGER,
             area TEXT,
             FOREIGN KEY (student_id) REFERENCES student(id)
 
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS student_qualification (
+    db.run(`CREATE TABLE IF NOT EXISTS student_qualification (
             student_id INTEGER,
             qualification TEXT,
             FOREIGN KEY (student_id) REFERENCES student(id)
 
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS student_teacher (
+    db.run(`CREATE TABLE IF NOT EXISTS student_teacher (
             student_id INTEGER,
             teacher_id INTEGER,
 FOREIGN KEY (student_id) REFERENCES student(id),
 FOREIGN KEY (teacher_id) REFERENCES teacher(id)
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS course_teacher (
+    db.run(`CREATE TABLE IF NOT EXISTS course_teacher (
             course_id INTEGER,
             teacher_id INTEGER,
 FOREIGN KEY (course_id) REFERENCES course(id),
 FOREIGN KEY (teacher_id) REFERENCES teacher(id)
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS session (
+    db.run(`CREATE TABLE IF NOT EXISTS session (
             session TEXT,
             login TEXT
 
         )`);
 
-        db.run(`CREATE TABLE IF NOT EXISTS user (
+    db.run(`CREATE TABLE IF NOT EXISTS user (
             login TEXT,
             password TEXT,
             type TEXT
 
         )`);
 
-        const user = "admin";
-        const password = "123456"; // obfuscated
-        const type = "admin";
-        // Hash the password
-        bcrypt.hash(password, 10, (err, hash) => {
-            if (err) throw err;
+    /*const user = "admin";
+    const password = "XXXXXXX"; // obfuscated
+    const type = "admin";
+    // Hash the password
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) throw err;
 
-            // check if already exist
-            db.get("SELECT * FROM user WHERE login = ?", [user], (err, row) => {
-                if (err) {
-                    return console.error(err.message);
-                }
-                if (row) {
-                    console.log(`User ${user} already exists.`);
-                    return;
-                }
-            });
+      // check if already exist
+      db.get("SELECT * FROM user WHERE login = ?", [user], (err, row) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        if (row) {
+          console.log(`User ${user} already exists.`);
+          return;
+        }
 
-            // Insert into the database
-            db.run(
-                `INSERT INTO user (login, password, type) VALUES (?, ?, ?)`,
-                [user, hash, type],
-                function (err) {
-                    if (err) {
-                        return console.error(err.message);
-                    }
-                    console.log(`A row has been inserted with rowid ${this.lastID}`);
-                }
-            );
-        });
-    });
+        // Insert into the database
+        db.run(
+          `INSERT INTO user (login, password, type) VALUES (?, ?, ?)`,
+          [user, hash, type],
+          function (err) {
+            if (err) {
+              return console.error(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+          }
+        );
+      });
+    });*/
+  });
 }
